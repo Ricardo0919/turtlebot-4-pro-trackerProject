a063e71eaa769d946ee53b5da2292e2591a595e6
Ricardo0919 <rickisierra03@gmail.com>
2025-11-04 00:05:53 +0000

    Update

--- tracker_ws/build/tracking_person/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ricardosierra/Documents/TEC/7Semestre/ApplicationProject/turtlebot-4-pro-trackerProject/tracker_ws/install/tracking_person'
